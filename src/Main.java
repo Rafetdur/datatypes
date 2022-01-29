b8ed0dc541c576d0235a1a524dc5514e8ebb9aa4
rafet <83957086+Rafetdur@users.noreply.github.com>
2022-01-29 12:45:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        double sayi = 12.5; // ondalik sayilar
+
+        sayi= -129;
+
+        char karakter = 'A'; //bir değişken, belli bir anda bir tek karekter tutabilir.
+
+        boolean dogruMu=false; // true or false
+
+    }
+}
